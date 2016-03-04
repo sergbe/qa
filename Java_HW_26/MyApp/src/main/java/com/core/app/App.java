ac2625f8ce4a85a2415799fb09cd8e2cf3778b8a
Serg Bezuglyy <sergbe@outlook.com>
2016-03-03 20:25:49 -0800

    Completed HW 26, 27, 28

---
+++ Java_HW_26/MyApp/src/main/java/com/core/app/App.java
@@ -0,0 +1,13 @@
+package com.core.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Sergey Bezuglyy" );
+    }
+}
